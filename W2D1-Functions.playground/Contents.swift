71c5b931548d972a633bbdd8debf2bae04e14101
Nathan Hsu <nathan.hsu@icloud.com>
2017-03-01 16:30:02 -0800

    W2D1 Done
    Assignment Done

--- W2D1-Functions.playground/Contents.swift
+++ W2D1-Functions.playground/Contents.swift
@@ -17,14 +17,14 @@ func sayHello() {
 //: Notice that the playground doesn't show anything to the left of the `print` statement. That's because this code hasn't run. We've created a function called `sayHello`, and described what that function does, but we havn't run the `sayHello` function yet.
 //: In order to run it, we have to *call* the `sayHello` function. Let's do that now by uncommenting this line (We comment out a line by putting // in front of the line of code. This makes the playground not run this line of code. To uncomment a line remove the // from the front of it. This will allow the playground to run this line of code):
 
-//sayHello()
+sayHello()
 
 //: The power of writing a function is that we can now repeat this code whenever we want. Feel free to call `sayHello` a few times below:
 
 //: Now our `sayHello` function is pretty cool, but we might want to say hello to a specific person.
 //: Functions can take one or more "parameters" or inputs, and do something with that input. Take this example function:
 
-func greet(name: String) {
+func greet(_ name: String) {
     print("Ah! Hello, \(name)")
 }
 
@@ -35,22 +35,31 @@ func greet(name: String) {
 //: Try to call this function with your name, so it greets you.
 //:
 //: Hint: put your name, wrapped in quotes, between the parentheses `()`.
-
+greet("Nathan")
 
 
 
 //: ### Challenge 2
 //:
 //: Now take the code your wrote in the last playground to print out a grammatical description of our bicycleCount, and write a function that takes in an Int representing how many bicycles we have, and prints out a description of how many bicycles we have. You can call the function `pluralize`.
+func pluralize(_ bicycleCount: Int) {
+    if bicycleCount == 1 {
+        print("There is \(bicycleCount) bicycle")
+    } else if (bicycleCount == 0) {
+        print("There are zero bicycles")
+    } else {
+        print("There are \(bicycleCount) bicycles")
+    }
+}
 
 
 
 
 // Test it by uncommenting these lines:
 
-// pluralize(1)
-// pluralize(0)
-// pluralize(100)
+pluralize(1)
+pluralize(0)
+pluralize(100)
 
 //: Not only can functions take in parameters, but they can return values. So far we've only written functions that don't return anything, they just print stuff. Here is an example of a function that takes in an Int and returns an Int
 
@@ -68,15 +77,22 @@ var result = increment(input: 10)
 //:
 //: Using the `increment` function as an example, re-write the `pluralize` function (call it `pluralizedString`) so it returns a String instead of printing.
 
-
-
+func pluralizedString(_ bicycleCount: Int) -> String {
+    if bicycleCount == 1 {
+        return "There is \(bicycleCount) bicycle"
+    } else if (bicycleCount == 0) {
+        return "There are zero bicycles"
+    } else {
+        return "There are \(bicycleCount) bicycles"
+    }
+}
 
 
 // Test it by uncommenting the lines below:
 
-//var str1 = pluralizedString(1)
-//var str2 = pluralizedString(0)
-//var str3 = pluralizedString(100)
+var str1 = pluralizedString(1)
+var str2 = pluralizedString(0)
+var str3 = pluralizedString(100)
 
 
 //: ## Bonus Challenge
@@ -96,15 +112,23 @@ var result = increment(input: 10)
 //:
 
 
-
+func pluralizedWord(_ singular: String, pluralNoun: String, count: Int) -> String {
+    if count == 1 {
+        return "There is \(count) \(singular)"
+    } else if (count == 0) {
+        return "There are zero \(pluralNoun)"
+    } else {
+        return "There are \(count) \(pluralNoun)"
+    }
+}
 
 
 
 // Test your function by uncommenting:
 
-// pluralizedWord("octopus", pluralNoun: "octopi", count: 1)
-// pluralizedWord("woman", pluralNoun: "women", count: 2)
-// pluralizedWord("apple", pluralNoun: "apples", count: 0)
+pluralizedWord("octopus", pluralNoun: "octopi", count: 1)
+pluralizedWord("woman", pluralNoun: "women", count: 2)
+pluralizedWord("apple", pluralNoun: "apples", count: 0)
 
 // You might have noticed that when we have a function that takes multiple inputs, we have to explicitly write out all input names other than the first one e.g. `pluralNoun` and `count`, seen above.
 
